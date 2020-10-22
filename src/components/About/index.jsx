6fd297113cab08f88424dc96048d90116a3ef787
Simranjot Kaur <200425455@student.georgianc.on.ca>
2020-10-21 21:20:04 -0400

    v

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../shared/Header';
 
+
 const Home = () => {
   return (
     <>
+	
       {/* You need to use your shared header component... */}
+		<Header title='About'/>
       {/* You need a paragraph describing a fun fact about you */}
+	  <p>I am afraid of dogs</p>
     </>
   );
 }
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 // You need to import your shared header...
+import Header from '../shared/Header';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const Home = () => {
   return (
     <>
       {/* You need to use your shared header component... */}
+	        <Header title="This is Home Page of Lab 2"/>
       {/* You need a link to your about page as a call to action */}
+	        <Link to='/About'><Button>About</Button></Link>
     </>
   );
 }
